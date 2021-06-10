9ab82cf8e9bb6ab0bf98db520e1de5ba9fd9db5b
Zhang XiaoFei <1326336909@qq.com>
2021-06-10 16:32:01 +0800

    更新

--- src/views/common/component/form-component.vue
+++ src/views/common/component/form-component.vue
@@ -89,7 +89,7 @@
     data() {
       return {
         upload_headers: {},
-        upload_url: this.$http.adornUrl('/file/data'),
+        upload_url: this.$http.adornUrl('/file/picture'),
 
         formItemList:{},
         dataForm: {},
